28377f808545f19459d1f39efc86d9ce12767acc
meta-mihir-jain <mihir.jain@metacube.com>
2021-03-15 14:23:05 +0530

    INITIAL

---
+++ Assignment 1/src/Linkedlist/Main.java
@@ -0,0 +1,84 @@
+package Linkedlist;
+
+import java.util.Scanner;
+public class Main {
+
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args){
+		My_linked_list list = new My_linked_list();
+		while(true){
+		System.out.println("1.Add data to list");
+		System.out.println("2.Display List");
+		System.out.println("3.Rotate SubList");
+		System.out.println("4.Detect Loop");
+		System.out.println("5.create multivariable polyNomial");
+		System.out.println("6.Exit");
+		int choice = sc.nextInt();
+		switch(choice){
+		case 1:
+			System.out.println("Enter total number of data");
+			int totalData = sc.nextInt();
+			for(int index = 0;index < totalData;index++){
+				list.add(sc.nextInt());
+			}
+			break;
+		case 2:
+			list.displayList();
+			break;
+		case 3:
+			System.out.println("Enter Left");
+			int left = sc.nextInt();
+			System.out.println("Enter right");
+			int right = sc.nextInt();
+			System.out.println("Enter number of Rotation");
+			int numberOfRotation = sc.nextInt();
+			list.rotateSubList(left, right, numberOfRotation);
+			break;
+		case 4:
+			System.out.println(list.detectLoop());
+			break;
+		case 5:
+			multiVariablePoly();
+		default:
+			return;
+		}
+		}
+	}
+	//handle the multivariable polynomail operation like getDegree or displayPolyNomail
+	private static void multiVariablePoly() {
+		MultiVariablePolynomial poly = new MultiVariablePolynomial();
+		while(true){
+			System.out.println("1.Add polynomial term");
+			System.out.println("2.Display Polynomial");
+			System.out.println("3.get polynomial degree");
+			System.out.println("4.exit");
+			int choice  = sc.nextInt();
+			switch(choice){
+			case 1:
+				System.out.println("Enter number of variable of "+poly.len+" term");
+				int var = sc.nextInt();
+				int [] expo = new int[var];
+				System.out.println("Enter coeff. of "+poly.len+" term");
+				int coeff = sc.nextInt();
+				for(int index=0;index<var;index++){
+					System.out.println("Enter exponent of "+poly.availableVar[index]);
+					expo[index] = sc.nextInt();
+				}
+				poly.addterm(coeff,expo);
+			break;
+			case 2:
+				System.out.println(poly.displayPoly());
+				break;
+			case 3:
+				System.out.println("\nDegree :"+poly.getDegree()+"\n");
+				break;
+			default:
+				return;
+				
+				
+			}
+			
+		}
+		
+	}
+}
---
+++ Assignment 1/src/Linkedlist/My_linked_list.java
@@ -0,0 +1,192 @@
+package Linkedlist;
+import java.util.HashMap;
+//Node class contains the data and address of the next node
+class Node{
+	int data;
+	Node nextNode ;
+	Node(int data){
+		this.data = data;
+		nextNode = null;
+	}
+}
+class MultiVariableNode{
+	int coeff;
+	char var;
+	int exp;
+	MultiVariableNode nextNode ;
+	MultiVariableNode downNode;
+	MultiVariableNode(int coeff,char var,int exp){
+		this.coeff = coeff;
+		this.exp = exp;
+		this.var = var;
+		downNode = null;
+		nextNode = null;
+	}
+}
+/*
+* this class provide basic methods to perform different operation on linked list
+* 1. addData to list @param data:int
+* 2.DisplayList
+* 3.Rotate a sublinkList @param left ,right and numberOfroation
+*/
+public class My_linked_list
+{
+ private Node start=null,tail=null;
+ //add data to the linked list
+	void add(int data){
+		Node newNode = new Node(data);
+		if(start == null){
+			start = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.nextNode = newNode;
+			tail = newNode;
+		}
+	}
+	//Display the data in the linked List
+	void displayList()
+	{
+		if(start == null)
+		{
+			System.out.println("List is Empty");
+			return;
+		}
+	
+		Node temp = start;
+		while(temp!=null){
+			System.out.print(temp.data+" ");
+			temp = temp.nextNode;
+		}
+		System.out.println();
+	}
+	//this function rotates the sublist by numberOfRotation
+	void rotateSubList(int left,int right,int numberOfRotation){
+		Node leftAddr = start;
+		Node rightAddr =  start;
+		Node rotationAddr = start;
+		int rotationIndex = right - (numberOfRotation%(right-left));
+		while(--left>1){
+			leftAddr = leftAddr.nextNode;
+		}
+		while(--right>0){
+			rightAddr = rightAddr.nextNode;
+		}		
+		while(--rotationIndex>0){
+			rotationAddr = rotationAddr.nextNode;
+		}
+		
+		Node tempAddr = rotationAddr.nextNode;
+		rotationAddr.nextNode = rightAddr.nextNode;
+		rightAddr.nextNode = leftAddr.nextNode;
+		leftAddr.nextNode = tempAddr;
+		}
+		boolean detectLoop()
+		{
+		HashMap<Node,Integer> map =  new HashMap<>();
+		Node start  = this.start;
+		while(start.nextNode!=null){
+			if(map.containsKey(start))
+			{
+				return true;
+			}
+			map.put(start, 1);
+			start = start.nextNode;
+		}
+		return false;
+	}
+}
+
+
+//This class handle work related to multivariable polynomial
+/*
+* This class provide following methods:
+* 1. addterm - add the term to the existing polynomail
+* 2. display Node - return the single term as string
+* 3. displayPoly -  add all the return term by displayNode and return the whole polyNomial
+*/
+
+class MultiVariablePolynomial{
+	
+	private MultiVariableNode tail = null ,head = null;
+	char[] availableVar = {'X','Y','Z','A','B','C','P'};
+	int len = 0;
+	private int degree=0;
+	
+	
+	
+	//add term to the polynomial 
+	//@param - coefficient ,  exponent array of all the variable
+	void addterm(int coeff,int[] expo) {
+		if(coeff == 0){
+			return;
+		}
+		MultiVariableNode node = new MultiVariableNode(coeff,availableVar[0],expo[0]);
+		int degree = expo[0];
+		for(int index=1;index<expo.length;index++){
+				MultiVariableNode tempNode = node;
+				while(tempNode.downNode!=null){
+					tempNode = tempNode.downNode;
+			}
+				tempNode.downNode = new MultiVariableNode(1,availableVar[index],expo[index]);
+				degree+=expo[index];
+				
+		}
+		if(this.degree < degree) this.degree = degree;
+		if(head==null){
+			head = node;
+			tail = node;
+		}
+		else{
+			tail.nextNode = node;
+			tail = node;
+		}
+		
+		len++;
+	}
+	
+	
+	//return the degree of the polynomial
+	int getDegree(){
+		return degree;
+	}
+	
+	
+	//return the string representaion of single Node
+	String displayNode(MultiVariableNode node){
+		MultiVariableNode tempNode = node;
+		MultiVariableNode tempDownNode = tempNode.downNode;
+			String term ="";
+			term +=""+tempNode.coeff+tempNode.var+"^"+tempNode.exp;
+			while(tempDownNode!=null){
+				term += tempDownNode.var+"^"+tempDownNode.exp;
+				tempDownNode = tempDownNode.downNode;
+			}
+//			term = term.replaceAll("^1","");
+			if(!term.contains("-") && tempNode!=head){
+				return "+"+term;
+			}
+			else{
+				return term;
+			}
+			
+		}
+	
+	
+	//return the representation of polynomial
+	String displayPoly(){
+		MultiVariableNode tempNode = head;
+		String resultPoly = "";
+		if(head == null){
+			return "First Add terms";
+		}
+		else{
+			while(tempNode!=null){
+				resultPoly += displayNode(tempNode);
+				tempNode = tempNode.nextNode;
+			}
+		}
+		return resultPoly;
+	}
+	
+}
